d942959b5bbc4765aef19f965f8e9ebfb1bcad48
Barry <bh375@cornell.edu>
2020-04-21 19:54:13 -0700

    post login

--- backend/index.js
+++ backend/index.js
@@ -17,4 +17,17 @@ const userTable = db.collection("Users");
 const stockTable = db.collection("UserStocks");
 const transactionTable = db.collection("Transactions");
 
+//json fields should be username, password
+//we will set balance to 0 during account creation 
+app.post('/signup/', function (req, res, next) {
+  const loginInfo = req.body;
+  if (loginInfo.Username == null || loginInfo.Password == null) {
+    res.status(500).send("Invalid Username or Password");
+  }
+  loginInfo.balance = 0;
+  const newDoc = userTable.doc(loginInfo.Username);
+  newDoc.set(loginInfo);
+  res.status(201).send(newDoc.Username);
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`));
