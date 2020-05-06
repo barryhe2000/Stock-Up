15b3f43578b19bd68d1f99d695750b725d590423
Samantha Liu <sl2486@cornell.edu>
2020-05-06 16:35:41 -0400

    changed home page text

--- stockweb/src/Home.jsx
+++ stockweb/src/Home.jsx
@@ -2,21 +2,27 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Authentication from './Authentication';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 export default () => {
+    
     return (
         <div className="home">
             <h1 className="logo">Stock-Up</h1>
             <Jumbotron id="jumbotron">
-            <h1>Investing Made Easy.</h1>
+            <h1>Budgeting Made Easy.</h1>
             <p>
-                Manage your stocks, make new investments, and get real-time data all in one place.
+                Create a budget, manage expenses, and track spending all in one place.
             </p>
             <p>
-                <Button id="custom-home-button" variant="outline-dark" href="/Home">Learn more</Button>
+                <Authentication />
             </p>
+            <button onCLick={() => firebase.auth().signOut()}>Sign Out</button>
             </Jumbotron>
-            <img src={require('./images/gradient.png')} alt="hi"></img>
+            {firebase.user && <div>HELLO</div>}
+            <img className="home-img" src={require('./images/gradient.png')} alt="hi"></img>
         </div>
     )
 }
