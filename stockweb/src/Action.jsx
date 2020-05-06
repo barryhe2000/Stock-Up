5b94f36b1103abe4a5183ec4ad560bcb2248dc00
Samantha Liu <sl2486@cornell.edu>
2020-05-06 16:35:59 -0400

    changed action page button text

--- stockweb/src/Action.jsx
+++ stockweb/src/Action.jsx
@@ -1,9 +1,12 @@
 import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 export default () => {
     return (
         <div>
+            
             <div className="action-header">
                 <p className="action-title">Hi, Alice.</p>
                 <p className="action-subtitle">What would you like to do?</p>
@@ -12,17 +15,17 @@ export default () => {
             <div className="action-options">
                 <div className="option-make-transaction">
                     <img className="action-transaction-img" src={require('./images/134.png')} alt="Girl Holding Bags"/>
-                    <Button id="option-transaction-button" variant="outline-dark" href="/Home">Make Transaction</Button>
+                    <Button id="option-transaction-button" variant="outline-dark" href="/Home">Input Expense</Button>
                 </div>
 
                 <div className="option-overview">
                     <img className="overview-img" src={require('./images/075.png')} alt="Man Checking Screen"/>
-                    <Button id="option-overview-button" variant="outline-dark" href="/Home">See Overview</Button>
+                    <Button id="option-overview-button" variant="outline-dark" href="/Home">Track Spending</Button>
                 </div>
 
                 <div className="option-add-funds">
                     <img className="add-funds-img" src={require('./images/104.png')} alt="Man Adding Funds"/>
-                    <Button id="option-funds-button" variant="outline-dark" href="/Home">Add Funds</Button>
+                    <Button id="option-funds-button" variant="outline-dark" href="/Home">Manage Budget</Button>
                 </div>
             
 
