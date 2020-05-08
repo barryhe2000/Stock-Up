a676c1e74a8bec9a4eb493885bde8a98c6535ba3
Samantha Liu <sl2486@cornell.edu>
2020-05-07 21:56:16 -0400

    render input expense form and image

---
+++ stockweb/src/InputExpense.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import NavBar from './NavBar';
+import InputExpenseForm from './InputExpenseForm';
+import {Redirect} from 'react-router-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default (loggedIn) => {
+    // REMEMBER TO PASS IN CALLBACK INTO INPUTEXPENSEFORM LATER
+
+    const handleInput = () => {
+
+    }
+
+    if (loggedIn) {
+
+        return (
+            <div>
+                <div className="nav-component">
+                    <NavBar active_page="input" />
+                </div>
+
+                <div className="input-expense-header">
+                    <p className="input-expense-title">Input Expense</p>
+                </div>
+
+                <div className="input-expense-component">
+                    <InputExpenseForm handleInput={handleInput} />
+                </div>
+
+                <div className="input-image-container">
+                    <img className="input-expense-image" src={require('./images/input_expense.png')} 
+                    alt="Man Sitting on Chair"></img>
+                </div>
+
+                
+
+            </div>
+
+        )
+
+    } else {
+
+        
+    }
+}
