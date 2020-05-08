607959d09489cb07aa63ae3feb5b05bd1f5d9bd7
Samantha Liu <sl2486@cornell.edu>
2020-05-07 21:44:33 -0400

    input expense form

--- stockweb/src/InputExpenseForm.jsx
+++ stockweb/src/InputExpenseForm.jsx
@@ -1,42 +1,123 @@
 import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-export default ({callback}) => {
+export default ({handleInput}) => {
 
-    const [stock, setStock] = useState('');
-    const [username, setUsername] = useState('');
-    const [quantity, setQuantity] = useState(0);
-    const [buy, setBuy] = useState('');
+    const [category, setCategory] = useState('');
+    const [amount, setAmount] = useState(0);
+    const [month, setMonth] = useState(0);
+    const [day, setDay] = useState(0);
+    const [year, setYear] = useState(0);
+    const [description, setDescription] = useState('');
+
+    const [validated, setValidated] = useState(false);
+
+    // handle form submit
+    const handleSubmit = (event) => {
+        let form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        } 
+
+        setValidated(true);
+        handleInput();
+        
+
+    };
 
     return (
         <div>
-            <Form className="transaction-form">
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label className="transaction-form-label">Stock Name</Form.Label>
-                    <Form.Control type="text" placeholder="e.g. TSLA, AAPL" 
-                    onChange={(e) => setStock(e.target.value)}
-                    />
-                </Form.Group>
+            <Form className="input-expense-form" noValidate 
+            validated={validated} onSubmit={handleSubmit} >
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label className="transaction-form-label">Quantity</Form.Label>
-                    <Form.Control type="text" placeholder="e.g. 1, 3, 8" 
-                    onChange={(e) => setQuantity(e.target.value)}
-                    />
-                </Form.Group>
-
+                {/* category */}
                 <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label className="transaction-form-label">Transaction Type</Form.Label>
-                    <Form.Control as="select" onChange={(e) => setBuy(e.target.value)}>
-                    <option>Buy</option>
-                    <option>Sell</option>
-                    </Form.Control>
+                        <Form.Label className="expense-form-label">Category</Form.Label>
+                        <Form.Control as="select" 
+                        onChange={(e) => setCategory(e.target.value)}>
+                        <option>Bills</option>
+                        <option>Entertainment</option>
+                        <option>Food</option>
+                        <option>Home</option>
+                        <option>Shopping</option>
+                        <option>Transport</option>
+                        <option>Other</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                {/* amount */}
+                <Form.Group controlId="formBasicEmail" className="input-expense-amount">
+                    <Form.Label className="expense-form-label">Amount</Form.Label>
+                        <InputGroup >
+
+                        {/* prepend $ symbol */}
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control type="text" placeholder="e.g. 100.00" 
+                        onChange={(e) => setAmount(parseInt(e.target.value))}
+                        aria-describedby="inputGroupPrepend"
+                        required
+                        />
+                        {/* feedback */}
+                        <Form.Control.Feedback type="invalid">
+                        Please enter a valid amount.
+                        </Form.Control.Feedback>
+                    </InputGroup>
                 </Form.Group>
 
-                <Button id="transaction-submit" variant="white" type="submit"
-                >
-                    Make Transaction
+                {/* month */}
+                <Form.Group controlId="formBasicPassword" className="input-expense-month">
+                    <Form.Label className="expense-form-label">Month</Form.Label>
+                    <Form.Control type="text" placeholder="MM" 
+                    onChange={(e) => setMonth(parseInt(e.target.value))} required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                    Please enter a valid month.
+                    </Form.Control.Feedback>
+                </Form.Group>
+            
+                {/* day */}
+                <Form.Group controlId="formBasicPassword" className="input-expense-day">
+                    <Form.Label className="expense-form-label">Day</Form.Label>
+                    <Form.Control type="text" placeholder="DD" 
+                    onChange={(e) => setDay(parseInt(e.target.value))} required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                    Please enter a valid day.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                {/* year */}
+                <Form.Group controlId="formBasicPassword" className="input-expense-year">
+                    <Form.Label className="expense-form-label">Year</Form.Label>
+                    <Form.Control type="text" placeholder="YYYY" 
+                    onChange={(e) => setYear(parseInt(e.target.value))} required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                    Please enter a valid year.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                {/* description */}
+                <Form.Group controlId="formBasicPassword" className="input-expense-desc">
+                    <Form.Label className="expense-form-label">Description</Form.Label>
+                    <Form.Control type="text" placeholder="e.g. Electricity bill" 
+                    onChange={(e) => setDescription(parseInt(e.target.value))} required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                    Please enter a valid description.
+                    </Form.Control.Feedback>
+                </Form.Group>
+
+                {/* description */}
+                <Button id="input-expense-submit" 
+                variant="white" type="submit" >
+                    Input Expense
                 </Button>
             </Form>
 
