81f756d3c1d59a46b8af8d5286d1071e81454922
Samantha Liu <sl2486@cornell.edu>
2020-05-12 19:26:29 -0400

    port

--- backend/index.js
+++ backend/index.js
@@ -202,4 +202,4 @@ app.get('/gettransportactions/:username', async (req, res, next) => {
     (item) => ({ ...item.data() })));
 })
 
-app.listen(process.env.port || port, () => console.log(`Listening on port ${port}!`));
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}!`));
